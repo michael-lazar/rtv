457ce39a07bf6b978ba70c681f1316cd26bb2f47
Pierre Arnout <p.arnout@esystema.fr>
2015-04-29 10:33:40 +0200

    add config._config_colors variable as default colors

--- rtv/config.py
+++ rtv/config.py
@@ -3,3 +3,18 @@ Global configuration settings
 """
 
 unicode = False
+
+_config_colors = {
+    'Header':      ( -1,  0 ),
+    'Title':       ( -1,  0 ),
+    'Link':        ( -1,  0 ),
+    'LinkSeen':    ( -1,  0 ),
+    'Author':      ( -1,  0 ),
+    'IsAuthor':    ( -1,  0 ),
+    'Subreddit':   ( -1,  0 ),
+    'Flair':       ( -1,  0 ),
+    'Gold':        ( -1,  0 ),
+    'LikesBullet': ( -1,  0 ),
+    'LikesUArrow': ( -1,  0 ),
+    'LikesDArrow': ( -1,  0 ),
+}
