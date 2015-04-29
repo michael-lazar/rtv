8accddd4de76ef6da3cfec0a3254d76af73c50f5
Pierre Arnout <p.arnout@esystema.fr>
2015-04-29 12:24:03 +0200

    add fiew colors in config.py

--- rtv/config.py
+++ rtv/config.py
@@ -4,17 +4,23 @@ Global configuration settings
 unicode = False
 
 default_colors = {
-    'Header':      ( -1,  0 ),
-    'Title':       ( -1,  0 ),
-    'Link':        ( -1,  0 ),
-    'LinkSeen':    ( -1,  0 ),
-    'Author':      ( -1,  0 ),
-    'IsAuthor':    ( -1,  0 ),
-    'Subreddit':   ( -1,  0 ),
-    'Flair':       ( -1,  0 ),
-    'Gold':        ( -1,  0 ),
-    'LikesBullet': ( -1,  0 ),
-    'LikesUArrow': ( -1,  0 ),
-    'LikesDArrow': ( -1,  0 ),
+    'CommentsText':      ( -1, 0 ),
+    'Header':            ( -1, 0 ),
+    'Title':             ( -1, 0 ),
+    'Link':              ( -1, 0 ),
+    'LinkSeen':          ( -1, 0 ),
+    'Author':            ( -1, 0 ),
+    'IsAuthor':          ( -1, 0 ),
+    'Subreddit':         ( -1, 0 ),
+    'Flair':             ( -1, 0 ),
+    'Gold':              ( -1, 0 ),
+    'LikesBullet':       ( -1, 0 ),
+    'LikesUArrow':       ( -1, 0 ),
+    'LikesDArrow':       ( -1, 0 ),
+    'Score':             ( -1, 0 ),
+    'Created':           ( -1, 0 ),
+    'Comments':          ( -1, 0 ),
+    'MoreComments':      ( -1, 0 ),
+    'MoreCommentsCount': ( -1, 0 )
 }
 
