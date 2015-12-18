e13c0c1b8a65903e0b9f3c6fde17ee7718c18ca8
PESCHE Romain <romain.pesche@gmail.com>
2015-12-18 19:45:24 +0100

    Keymap class to handle key bindings

---
+++ rtv/keymap.py
@@ -0,0 +1,100 @@
+import re
+from .page import Page
+from .subreddit import SubredditPage
+from .submission import SubmissionPage
+from .subscription import SubscriptionPage
+
+class UnknownBinding(Exception):
+    pass
+
+
+class KeyMap(dict):
+
+    default_bindings = {
+        'main-exit': 'q',
+        'main-force_exit': 'Q',
+        'main-show_help': '?',
+        'main-sort_content_hot': '1',
+        'main-sort_content_top': '2',
+        'main-sort_content_rising': '3',
+        'main-sort_content_new': '4',
+        'main-sort_content_controversial': '5',
+        'main-move_cursor_up': 's',
+        'main-move_cursor_down': 't',
+        'main-move_page_up': 'm',
+        'main-move_page_down': 'n',
+        'main-upvote': 'a',
+        'main-downvote': 'z',
+        'main-login': 'u',
+        'main-delete_item': 'd',
+        'main-edit': 'e',
+        'main-get_inbox': 'i',
+
+        'submission-toggle_comment': 'l',
+        'submission-exit_submission': 'h',
+        'submission-refresh_content': 'r',
+        'submission-open_link': 'o',
+        'submission-add_comment': 'c',
+        'submission-delete_comment': 'd',
+
+        'subreddit-refresh_content': 'r',
+        'subreddit-search_subreddit': 'f',
+        'subreddit-prompt_subreddit': '/',
+        'subreddit-open_submission': 'l',
+        'subreddit-open_link': 'o',
+        'subreddit-post_submission': 'c',
+        'subreddit-open_subscriptions': 's',
+
+        'subscription-refresh_content': 'r',
+        'subscription-select_subreddit': 'l',
+        'subscription-close_subscriptions': 'h'
+    }
+
+    userPageMap = {}
+    userSubredditMap = {}
+    userSubscriptionMap = {}
+    userSubmissionMap = {}
+
+    def __init__(self, userMap=None):
+        super(KeyMap, self).__init__()
+
+        if userMap:
+            self.loadUserMap(userMap)
+
+    def __setitem__(self, binding, key):
+        self.bind(binding, key)
+
+    def loadUserMap(self, userMap):
+        for bind in userMap:
+            try:
+                self.bind(bind, userMap[bind])
+            except UnknownBinding:
+                print("binding "+bind+" Don't exist")
+
+    def addUserBinding(self, key, keymap, function):
+        if key not in keymap:
+            keymap[ord(key[0])] = function
+        else:
+            print(key+" is mapped multiple time")
+
+    def bind(self, binding, key):
+
+        if binding in self.default_bindings:
+            keymap, function = self.binding2function(binding)
+            self.addUserBinding(key, keymap, function)
+        else:
+            raise UnknownBinding()
+
+    def binding2function(self, binding):
+        """ Get the keymap and method reference of a binding's string """
+
+        s = binding.split('-')
+
+        if re.match('^main-', binding):
+            return (self.userPageMap, eval('Page.'+s[1]))
+        if re.match('^submission-', binding):
+            return (self.userSubmissionMap, eval('SubmissionPage.'+s[1]))
+        if re.match('^subreddit-', binding):
+            return (self.userSubredditMap, eval('SubredditPage.'+s[1]))
+        if re.match('^subscription-', binding):
+            return (self.userSubscriptionMap, eval('SubscriptionPage.'+s[1]))
