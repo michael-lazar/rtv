1f216e833a2e19f718332855fa3fc37e0901172e
PESCHE Romain <romain.pesche@gmail.com>
2016-01-03 20:35:38 +0100

    Default keys in keymap can be multiple and integer

--- rtv/keymap.py
+++ rtv/keymap.py
@@ -1,9 +1,12 @@
 import re
 import six
+import curses
+
 from .page import Page, PageController
 from .subreddit import SubredditPage, SubredditController
 from .submission import SubmissionPage, SubmissionController
 from .subscription import SubscriptionPage, SubscriptionController
+from .terminal import Terminal
 
 
 class UnknownBinding(Exception):
@@ -17,43 +20,43 @@ class NonStandardKey(Exception):
 class KeyMap(dict):
 
     default_bindings = {
-        'main-exit': 'q',
-        'main-force_exit': 'Q',
-        'main-show_help': '?',
-        'main-sort_content_hot': '1',
-        'main-sort_content_top': '2',
-        'main-sort_content_rising': '3',
-        'main-sort_content_new': '4',
-        'main-sort_content_controversial': '5',
-        'main-move_cursor_up': 'k',
-        'main-move_cursor_down': 'j',
-        'main-move_page_up': 'm',
-        'main-move_page_down': 'n',
-        'main-upvote': 'a',
-        'main-downvote': 'z',
-        'main-login': 'u',
-        'main-delete_item': 'd',
-        'main-edit': 'e',
-        'main-get_inbox': 'i',
+        'main-exit': ['q'],
+        'main-force_exit': ['Q'],
+        'main-show_help': ['?'],
+        'main-sort_content_hot': ['1'],
+        'main-sort_content_top': ['2'],
+        'main-sort_content_rising': ['3'],
+        'main-sort_content_new': ['4'],
+        'main-sort_content_controversial': ['5'],
+        'main-move_cursor_up': ['k'],
+        'main-move_cursor_down': ['j'],
+        'main-move_page_up': ['m'],
+        'main-move_page_down': ['n'],
+        'main-upvote': ['a'],
+        'main-downvote': ['z'],
+        'main-login': ['u'],
+        'main-delete_item': ['d'],
+        'main-edit': ['e'],
+        'main-get_inbox': ['i'],
 
-        'submission-toggle_comment': 'l',
-        'submission-exit_submission': 'h',
-        'submission-refresh_content': 'r',
-        'submission-open_link': 'o',
-        'submission-add_comment': 'c',
-        'submission-delete_comment': 'd',
+        'submission-toggle_comment': ['l'],
+        'submission-exit_submission': ['h'],
+        'submission-refresh_content': ['r'],
+        'submission-open_link': ['o', Terminal.RETURN],
+        'submission-add_comment': ['c'],
+        'submission-delete_comment': ['d'],
 
-        'subreddit-refresh_content': 'r',
-        'subreddit-search_subreddit': 'f',
-        'subreddit-prompt_subreddit': '/',
-        'subreddit-open_submission': 'l',
-        'subreddit-open_link': 'o',
-        'subreddit-post_submission': 'c',
-        'subreddit-open_subscriptions': 's',
+        'subreddit-refresh_content': ['r'],
+        'subreddit-search_subreddit': ['f'],
+        'subreddit-prompt_subreddit': ['/'],
+        'subreddit-open_submission': ['l'],
+        'subreddit-open_link': ['o'],
+        'subreddit-post_submission': ['c'],
+        'subreddit-open_subscriptions': ['s'],
 
-        'subscription-refresh_content': 'r',
-        'subscription-select_subreddit': 'l',
-        'subscription-close_subscriptions': 'h'
+        'subscription-refresh_content': ['r'],
+        'subscription-select_subreddit': ['l', curses.KEY_ENTER],
+        'subscription-close_subscriptions': ['h']
     }
 
     userPageMap = {}
@@ -147,11 +150,14 @@ class KeyMap(dict):
         return base_class, function, controller, keymap
 
     def fillWithDefaultKey(self):
-        for binding, key in self.default_bindings.items():
+        for binding, keys in self.default_bindings.items():
             keymap = self.bindingKeyMap(binding)
             if binding not in keymap.values():
-                print("Add default binding "+binding)
-                keymap[ord(key[0])] = binding
+                for key in keys:
+                    if isinstance(key, six.text_type):
+                        keymap[ord(key[0])] = binding
+                    else:
+                        keymap[key] = binding
 
     def emptyController(self):
         PageController.character_map = {}
