c40ac994869631036b28f1fc097fa6ab2109c180
woorst <woorst@users.noreply.github.com>
2016-07-22 18:41:09 -0500

    Pass full url_full to urlview if submission is external link

--- rtv/submission.py
+++ rtv/submission.py
@@ -150,7 +150,7 @@ class SubmissionPage(Page):
     @SubmissionController.register(Command('SUBMISSION_OPEN_IN_URLVIEWER'))
     def comment_urlview(self):
         data = self.content.get(self.nav.absolute_index)
-        comment = data.get('body') or data.get('text') or data.get('url')
+        comment = data.get('body') or data.get('text') or data.get('url_full')
         if comment:
             self.term.open_urlview(comment)
         else:
