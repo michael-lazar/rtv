713706f35d7ffb6c71e0c29103c5dd09c0df00cc
woorst <woorst@github.com>
2017-06-10 23:19:16 -0500

    Add ability to manage subscriptions

--- rtv/subscription_page.py
+++ rtv/subscription_page.py
@@ -7,6 +7,7 @@ from . import docs
 from .page import Page, PageController
 from .content import SubscriptionContent, SubredditContent
 from .objects import Color, Navigator, Command
+from .packages import praw
 
 
 class SubscriptionController(PageController):
@@ -43,17 +44,22 @@ class SubscriptionPage(Page):
             self.nav = Navigator(self.content.get)
 
     @SubscriptionController.register(Command('PROMPT'))
-    def prompt_subreddit(self):
-        "Open a prompt to navigate to a different subreddit"
+    def add_subreddit(self):
+        "Open a prompt to add or remove multi/subreddit"
 
-        name = self.term.prompt_input('Enter page: /')
+        context = self.content.name
+        listing = self.get_selected_item()['object']
+        name = self.term.prompt_input('Add: /')
         if name is not None:
-            with self.term.loader('Loading page'):
-                content = SubredditContent.from_name(
-                    self.reddit, name, self.term.loader)
-            if not self.term.loader.exception:
-                self.selected_subreddit = content
-                self.active = False
+            with self.term.loader('Adding {} to /r/{}'.format(name, context)):
+                if isinstance(listing, praw.objects.Multireddit):
+                    self.reddit.create_multireddit(name)
+                elif hasattr(self.content, '_multireddit'):
+                    adsf
+                    self.content._multireddit.add_subreddit(name)
+                elif isinstance(listing, praw.objects.Subreddit):
+                    self.reddit.get_subreddit(name).subscribe()
+                self.refresh_content()
 
     @SubscriptionController.register(Command('SUBSCRIPTION_SELECT'))
     def select_subreddit(self):
@@ -72,6 +78,39 @@ class SubscriptionPage(Page):
         "Close subscriptions and return to the subreddit page"
 
         self.active = False
+
+    @SubscriptionController.register(Command('DELETE'))
+    def delete_reddit(self):
+        "Delete the selected reddit"
+
+        context = self.content.name
+        data = self.get_selected_item()
+        listing = data['object']
+        name = data['name']
+        with self.term.loader('Deleting {} from {}'.format(name, context)):
+            if isinstance(listing, praw.objects.Multireddit):
+                modhash = self.reddit.modhash
+                self.reddit.modhash = ''
+                self.reddit.delete_multireddit(listing.name)
+                self.reddit.modhash = modhash
+            elif hasattr(self.content, '_multireddit'):
+                self.content._multireddit.remove_subreddit(name)
+            elif isinstance(listing, praw.objects.Subreddit):
+                listing.unsubscribe()
+            self.refresh_content()
+
+    @SubscriptionController.register(Command('SUBMISSION_TOGGLE_COMMENT'))
+    def open_multireddit(self):
+        "View content of multireddit"
+
+        context = self.content.name
+        multi = self.get_selected_item()['object']
+        if context == 'My Multireddits':
+            with self.term.loader():
+                self.content = SubscriptionContent.from_multireddit(
+                    self.reddit, self.term.loader, multi)
+            if not self.term.loader.exception:
+                self.nav = Navigator(self.content.get)
 
     def _draw_banner(self):
         # Subscriptions can't be sorted, so disable showing the order menu
