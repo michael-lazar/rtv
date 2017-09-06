4b45871327dda6ea6e3f5b880d83f4cb4d0435f2
Michael Lazar <michael.lazar.ctr@dren.hpc.mil>
2017-09-06 19:49:23 -0400

    Delete non-empty directories

--- scripts/pip_clean.sh
+++ scripts/pip_clean.sh
@@ -3,7 +3,7 @@
 
 find . -type f -name '*.pyc' -delete
 find . -type f -name '*.pyo' -delete
-find . -type d -name '__pycache__' -delete
-find . -type d -name 'build' -delete
-find . -type d -name 'dist' -delete
-find . -type d -name '*.egg-info' -delete
+find . -type d -name '__pycache__' -exec rm -rv {} +
+find . -type d -name 'build' -exec rm -rv {} +
+find . -type d -name 'dist' -exec rm -rv {} +
+find . -type d -name '*.egg-info' -exec rm -rv {} +
